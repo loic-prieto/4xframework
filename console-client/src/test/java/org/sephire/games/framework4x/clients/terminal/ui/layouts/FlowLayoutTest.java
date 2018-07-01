156a1cab45d30db747646d0c0c893eb6dffc0b3b
Loïc Prieto <loic.sephiroth@gmail.com>
2018-07-01 16:31:38 +0200

    Refactor test convenience classes and methods

--- console-client/src/test/java/org/sephire/games/framework4x/clients/terminal/ui/layouts/FlowLayoutTest.java
+++ console-client/src/test/java/org/sephire/games/framework4x/clients/terminal/ui/layouts/FlowLayoutTest.java
@@ -1,18 +1,14 @@
 package org.sephire.games.framework4x.clients.terminal.ui.layouts;
 
 import org.junit.jupiter.api.Test;
-import org.sephire.games.framework4x.clients.terminal.ui.Coordinates;
-import org.sephire.games.framework4x.clients.terminal.ui.Painter;
 import org.sephire.games.framework4x.clients.terminal.ui.components.Container;
 import org.sephire.games.framework4x.clients.terminal.ui.components.UIElement;
 import org.sephire.games.framework4x.clients.terminal.ui.layouts.flowlayout.Direction;
-import org.sephire.games.framework4x.clients.terminal.ui.size.QualifiedSizeValue;
-import org.sephire.games.framework4x.clients.terminal.ui.size.Size;
-import org.sephire.games.framework4x.core.model.map.Location;
+import org.sephire.games.framework4x.clients.terminal.ui.mocks.TestContainer;
 
 import static io.vavr.collection.List.range;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.sephire.games.framework4x.clients.terminal.ui.size.SizeUnit.CHARACTER;
+import static org.sephire.games.framework4x.clients.terminal.ui.mocks.TestUIElement.buildUIElement;
 
 public class FlowLayoutTest {
 
@@ -85,41 +81,4 @@ public class FlowLayoutTest {
 		assertEquals(7, e3.getCoordinates().getLocation().getY());
 	}
 
-	/**
-	 * Convenience method to build ui elements for flow layout testing.
-	 * Location is set to 0,0
-	 *
-	 * @param width
-	 * @param height
-	 * @return
-	 */
-	private UIElement buildUIElement(int width, int height) {
-		UIElement element = new TestUIElement();
-		element.setCoordinates(new Coordinates(
-				new Location(0, 0),
-				new Size(
-						new QualifiedSizeValue(width, CHARACTER),
-						new QualifiedSizeValue(10, CHARACTER))
-		));
-
-		return element;
-	}
-
-	/**
-	 * A basic test container that does nothing but contain items.
-	 */
-	private class TestContainer extends Container {
-		@Override
-		public void draw(Painter painter) {
-		}
-	}
-
-	/**
-	 * A basic test ui element that does not even draw itself.
-	 */
-	private class TestUIElement extends UIElement {
-		@Override
-		public void draw(Painter painter) {
-		}
-	}
 }
---
+++ console-client/src/test/java/org/sephire/games/framework4x/clients/terminal/ui/mocks/TestContainer.java
@@ -0,0 +1,13 @@
+package org.sephire.games.framework4x.clients.terminal.ui.mocks;
+
+import org.sephire.games.framework4x.clients.terminal.ui.Painter;
+import org.sephire.games.framework4x.clients.terminal.ui.components.Container;
+
+/**
+ * A basic test container that does nothing but contain items.
+ */
+public class TestContainer extends Container {
+	@Override
+	public void draw(Painter painter) {
+	}
+}
