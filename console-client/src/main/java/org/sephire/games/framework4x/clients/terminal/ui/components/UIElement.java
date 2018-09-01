865f481614a1b40ab51cd1cb5b3fa3e0a2e0887e
Loic Prieto <loic.sephiroth@gmail.com>
2018-09-01 13:21:04 +0200

    Provide each UI Element with an identifier, so that it can be compared
    For inclusion in lists and maps, we need the UI Element to be comparable,
    and so we've included an artificial autogenerated UUID for each element.
    We don't really care for the value of the identifier, just that it exists.

--- console-client/src/main/java/org/sephire/games/framework4x/clients/terminal/ui/components/UIElement.java
+++ console-client/src/main/java/org/sephire/games/framework4x/clients/terminal/ui/components/UIElement.java
@@ -1,6 +1,7 @@
 package org.sephire.games.framework4x.clients.terminal.ui.components;
 
 import io.vavr.control.Option;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
@@ -9,6 +10,8 @@ import org.sephire.games.framework4x.clients.terminal.ui.Coordinates;
 import org.sephire.games.framework4x.clients.terminal.ui.size.Size;
 import org.sephire.games.framework4x.core.model.map.Location;
 
+import java.util.UUID;
+
 import static org.sephire.games.framework4x.clients.terminal.ui.size.QualifiedSizeValue.FIXED_SIZE_ONE;
 
 /**
@@ -16,11 +19,14 @@ import static org.sephire.games.framework4x.clients.terminal.ui.size.QualifiedSi
  */
 @Getter
 @Setter
+@EqualsAndHashCode(of = "identifier" )
 public abstract class UIElement implements Drawable {
 	@NonNull
 	private Coordinates coordinates;
 	@NonNull
 	private Option<Container> containerParent;
+	@NonNull
+	private UUID identifier;
 
 	public UIElement() {
 		this(new Coordinates(new Location(0, 0), new Size(FIXED_SIZE_ONE, FIXED_SIZE_ONE)), null);
@@ -33,5 +39,6 @@ public abstract class UIElement implements Drawable {
 	public UIElement(Coordinates coordinates,Container containerParent) {
 		this.coordinates = coordinates;
 		this.containerParent = Option.of(containerParent);
+		this.identifier = UUID.randomUUID();
 	}
 }
