af29106f89d74832bd2492a09737b50e5a912803
Loic Prieto <loic.sephiroth@gmail.com>
2018-11-22 21:38:18 +0100

    Document a little bit the Function1 wrapper function

--- console-client/src/main/java/org/sephire/games/framework4x/clients/terminal/utils/Functions.java
+++ console-client/src/main/java/org/sephire/games/framework4x/clients/terminal/utils/Functions.java
@@ -6,6 +6,16 @@ import java.util.function.Consumer;
 
 public class Functions {
 
+	/**
+	 * Wraps a Consumer function into a Function1 lambda that returns null.
+	 * This is needed in the case that we want to have the signature of the
+	 * methods use vavr function classes with void return function, but still be able
+	 * to write functions that return nothing, which otherwise Function classes force you
+	 * to do it.
+	 * @param consumer
+	 * @param <T>
+	 * @return
+	 */
 	public static <T> Function1<T,Void> wrap(Consumer<T> consumer) {
 		Function1<T,Void> wrappedConsumer = (T item)-> {
 			consumer.accept(item);
