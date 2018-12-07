dc976506d1ddef386edd0f34bd09b97d76fd3aa0
Loïc Prieto <lprieto@thoughtworks.com>
2018-12-07 02:12:42 +0100

    Adapt lombok and the projects to java 11, many things do not work
    Also, since lombok and the libraries are automatic modules, jlink cannot
    be used to generate a reduced java image.
    
    The plugin loading mechanism has to be reworked, since we don't have
    access to a classpath anymore.
    
    I'm not able to build with intellij, but i can with maven.

---
+++ console-client/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.sephire.games.framework4x.clients.terminal {
+
+	requires org.sephire.games.framework4x.core;
+	requires org.sephire.games.framework4x.clients.terminal.api;
+
+	requires lanterna;
+	requires io.vavr;
+	requires static lombok;
+	requires slf4j.api;
+}
--- core/src/test/java/org/sephire/games/framework4x/core/ConfigTest.java
+++ core/src/test/java/org/sephire/games/framework4x/core/ConfigTest.java
@@ -1,28 +1,22 @@
 package org.sephire.games.framework4x.core;
 
-import com.yacl4j.core.ConfigurationBuilder;
-import com.yacl4j.core.source.optional.ConfigurationSourceNotAvailableException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.sephire.games.framework4x.core.plugins.configuration.ConfigLoader;
 import org.sephire.games.framework4x.core.plugins.configuration.PluginSpecMapping;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ConfigTest {
 
 	@Test
 	public void testConfigLoad() {
 		var mappingsFileLocation = "org/sephire/games/framework4x/testing/dummyPlugin/plugin.yaml";
-		PluginSpecMapping mappings = null;
-		try {
-			mappings = ConfigurationBuilder.newBuilder()
-			  .source().fromFileOnClasspath(mappingsFileLocation)
-			  .build(PluginSpecMapping.class);
-			Assertions.assertTrue(true);
-		} catch (ConfigurationSourceNotAvailableException e) {
-			Assertions.assertTrue(false);
-		} catch (Throwable t) {
-			Assertions.assertTrue(false);
-		}
 
-		Assertions.assertTrue(true);
+		var pluginConfig = ConfigLoader
+		  .getConfigFor(mappingsFileLocation,PluginSpecMapping.class);
+
+		assertTrue(pluginConfig.isSuccess());
+		assertEquals("org.sephire.games.framework4x.testing.dummyPlugin",pluginConfig.get().getName());
 	}
 }
