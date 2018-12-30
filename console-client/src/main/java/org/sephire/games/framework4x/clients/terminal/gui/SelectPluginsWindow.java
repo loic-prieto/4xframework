ec43f655341e0c967383d9334631e3a70738b6cb
Loic Prieto <lprieto@thoughtworks.com>
2018-12-30 21:54:59 +0100

    start implementation of plugin validation in select plugin window

--- console-client/src/main/java/org/sephire/games/framework4x/clients/terminal/gui/SelectPluginsWindow.java
+++ console-client/src/main/java/org/sephire/games/framework4x/clients/terminal/gui/SelectPluginsWindow.java
@@ -1,17 +1,24 @@
 package org.sephire.games.framework4x.clients.terminal.gui;
 
-import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.sephire.games.framework4x.core.plugins.Plugin;
 import org.sephire.games.framework4x.core.plugins.PluginManager;
+import org.sephire.games.framework4x.core.plugins.PluginSpec;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.googlecode.lanterna.gui2.LinearLayout.Alignment.Fill;
-import static org.sephire.games.framework4x.clients.terminal.utils.Terminal.Size.sizeWithHeightToPercent;
+import static java.util.function.Predicate.not;
 import static org.sephire.games.framework4x.clients.terminal.utils.Terminal.Size.sizeWithWidthToPercent;
 
 /**
@@ -28,12 +35,17 @@ public class SelectPluginsWindow extends BasicWindow {
 	private Label pluginInfoLabel;
 	private Label infoLabel;
 	private WindowBasedTextGUI textGUI;
+	// The purpose of this object is to store the position of plugin specs inside the checkbox, and to be able
+	// to search parents quickly.
+	private Map<String,PluginSpec> pluginCache;
+	private CheckBoxList<PluginSpec> pluginCheckBoxList;
+	private Button selectButton;
 
 	private final static String DEFAULT_DESCRIPTION = "Select a plugin to see it's description";
 	private final static String DEFAULT_INFO = "Select the desired plugins to load then press enter to open the create game window, or escape to go back to the menu";
 	private final static String PLUGIN_LIST_LABEL = "Select the plugins you wish to load:";
 
-	public SelectPluginsWindow(PluginManager pluginManager,WindowBasedTextGUI textGUI) {
+	public SelectPluginsWindow(PluginManager pluginManager, WindowBasedTextGUI textGUI) {
 		super("Plugin selection screen");
 		this.textGUI = textGUI;
 		this.pluginManager = pluginManager;
@@ -51,7 +63,7 @@ public class SelectPluginsWindow extends BasicWindow {
 		addWindowListener(new WindowListenerAdapter() {
 			@Override
 			public void onInput(Window basePane, KeyStroke keyStroke, AtomicBoolean deliverEvent) {
-				if(keyStroke.getKeyType() == KeyType.Escape) {
+				if (keyStroke.getKeyType() == KeyType.Escape) {
 					log.info("The escape key was pressed");
 					deliverEvent.set(false);
 					self.close();
@@ -81,21 +93,73 @@ public class SelectPluginsWindow extends BasicWindow {
 		pluginsListLabel.setLayoutData(LinearLayout.createLayoutData(Fill));
 		pluginListPanel.addComponent(pluginsListLabel);
 
-		var pluginsCheckboxList = new CheckBoxList<String>();
-		pluginsCheckboxList.setLayoutData(LinearLayout.createLayoutData(Fill));
-		pluginManager.getAvailablePluginsNames().forEach(pluginsCheckboxList::addItem);
-		pluginListPanel.addComponent(pluginsCheckboxList);
+		// Build the cache of plugins inside the checkbox
+		pluginCache = pluginManager.getAvailablePlugins()
+		  .map((p) -> Tuple.of(p.getPluginName(), p))
+		  .collect(HashMap.collector());
 
-		var nextStepButton = new Button("Select plugins");
-		nextStepButton.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.End));
-		pluginListPanel.addComponent(nextStepButton);
+		this.pluginCheckBoxList = new CheckBoxList<PluginSpec>();
+		pluginCheckBoxList.setLayoutData(LinearLayout.createLayoutData(Fill));
+		pluginCheckBoxList.setListItemRenderer(new CheckBoxList.CheckBoxListItemRenderer<>() {
+			@Override
+			public String getLabel(CheckBoxList<PluginSpec> listBox, int index, PluginSpec item) {
+				var label = (item.isBasePlugin() ? "[Base] " : "").concat(item.getPluginName());
+				log.trace("The get label method of the checkbox was called, with result {}",label);
+				return label;
+			}
+		});
+		pluginCheckBoxList.addListener((itemIndex, checked) -> {
+			var selectedPlugin = pluginCheckBoxList.getItemAt(itemIndex);
+			log.trace("The plugin {} was checked? {}",selectedPlugin.getPluginName(),checked);
+			if (checked && !selectedPlugin.isBasePlugin()) {
+				// If a child plugin is selected, check also the parent if found
+				var parentPluginSearch = pluginCache.get(selectedPlugin.getParentPlugin().get());
+				if (parentPluginSearch.isDefined()) {
+					pluginCheckBoxList.setChecked(parentPluginSearch.get(),true);
+				} else {
+					var errorMessage = "The parent of this plugin could not be found, make sure it is present in the plugins folder";
+					MessageDialog.showMessageDialog(textGUI,"Error",errorMessage, MessageDialogButton.OK);
+					log.error("A child plugin was selected but it's parent is not in the list of available plugins");
+					pluginCheckBoxList.setChecked(selectedPlugin,false);
+				}
+			} else {
+				// If a parent is unchecked, uncheck the children too
+				if(!checked && selectedPlugin.isBasePlugin()) {
+					pluginCache.values()
+					  .filter(not(PluginSpec::isBasePlugin))
+					  .filter((child)->child.getParentPlugin().get().equals(selectedPlugin.getPluginName()))
+					  .forEach((child)->pluginCheckBoxList.setChecked(child,false));
+				}
+			}
+
+			updateSelectButtonState();
+		});
+		pluginCache.values().forEach(pluginCheckBoxList::addItem);
+		pluginListPanel.addComponent(pluginCheckBoxList);
+		
+
+		selectButton = new Button("Start Creating Game");
+		selectButton.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.End));
+		selectButton.setEnabled(false);
+		pluginListPanel.addComponent(selectButton);
 
 		backgroundPanel.addComponent(pluginListPanel.withBorder(Borders.singleLine("Plugin list")));
 	}
 
+	/**
+	 * This method sets the state of the Selected button, which allows to go to the next step in creating
+	 * a game.
+	 * @return
+	 */
+	private void updateSelectButtonState() {
+		var isBasePluginSelected = pluginCheckBoxList.getCheckedItems().stream().anyMatch((p)->p.isBasePlugin());
+
+		selectButton.setEnabled(isBasePluginSelected);
+	}
+
 	private void buildPluginInfoPanel(Panel backgroundPanel) {
 		Panel pluginInfoPanel = new Panel();
-		pluginInfoPanel.setPreferredSize(sizeWithWidthToPercent(textGUI.getScreen().getTerminalSize(),0.4f));
+		pluginInfoPanel.setPreferredSize(sizeWithWidthToPercent(textGUI.getScreen().getTerminalSize(), 0.4f));
 		pluginInfoPanel.setLayoutData(BorderLayout.Location.RIGHT);
 		pluginInfoPanel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
 		pluginInfoLabel = new Label(DEFAULT_DESCRIPTION);
