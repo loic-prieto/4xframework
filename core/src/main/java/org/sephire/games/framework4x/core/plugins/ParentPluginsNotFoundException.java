b66c4b4552ed1d1dcd644ab813b6c8fcf4b8ca2e
Loic Prieto <lprieto@thoughtworks.com>
2019-01-26 08:57:46 +0100

    Update license to Affero GPL3 instead of LGPL-3

--- core/src/main/java/org/sephire/games/framework4x/core/plugins/ParentPluginsNotFoundException.java
+++ core/src/main/java/org/sephire/games/framework4x/core/plugins/ParentPluginsNotFoundException.java
@@ -3,16 +3,16 @@
  * Copyright © 2018 Loïc Prieto Dehennault (loic.sephiroth@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.sephire.games.framework4x.core.plugins;
@@ -21,7 +21,6 @@ import io.vavr.Tuple2;
 import io.vavr.collection.Set;
 import lombok.Getter;
 import org.sephire.games.framework4x.core.Framework4XException;
-import org.sephire.games.framework4x.core.utils.FunctionalUtils;
 import org.sephire.games.framework4x.core.utils.FunctionalUtils.Reduce;
 
 import static java.lang.String.format;
